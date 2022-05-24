47c4a77f98db6c52ac4b17aface63befd760a2ca
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2022-05-25 02:19:01 +0700

    Add winston logging

--- app/controller.js
+++ app/controller.js
@@ -1,3 +1,20 @@
+const winston = require('winston');
+require('winston-logstash');
+
+winston.add(new winston.transports.Logstash({
+  port: 10194,
+  host: '46844509-c4df-4418-b7db-61abd1ecd859-ls.logit.io',
+  ssl_enable: true,
+  max_connect_retries: -1,
+}));
+
+// winston.add(winston.transports.Logstash, {
+//   port: 10194,
+//   host: '46844509-c4df-4418-b7db-61abd1ecd859-ls.logit.io',
+//   ssl_enable: true,
+//   max_connect_retries: -1,
+// });
+
 exports.echo = (req, res) => {
   if (!req.query.param) {
     res.status(401).send({
@@ -8,6 +25,12 @@ exports.echo = (req, res) => {
   }
 
   const param = req.query.param;
+
+  // Not working, clone error, not supported?
+  // winston.error('This is a test error log message', { custom: 'my custom field', Environment: 'local' });
+  // winston.log("error", "This is a standard log message");
+  // winston.info("This is an info log message");
+  // winston.warn("This is a warning log message");
 
   res.send(param)
 };
