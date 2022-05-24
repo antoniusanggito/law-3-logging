2f5cef6cd6219ac2c7ba5b6a7d24fbac668a3f9c
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2022-05-25 03:02:16 +0700

    Fix using winston 2

--- app/controller.js
+++ app/controller.js
@@ -1,19 +1,12 @@
 const winston = require('winston');
 require('winston-logstash');
 
-winston.add(new winston.transports.Logstash({
+winston.add(winston.transports.Logstash, {
   port: 10194,
   host: '46844509-c4df-4418-b7db-61abd1ecd859-ls.logit.io',
   ssl_enable: true,
   max_connect_retries: -1,
-}));
-
-// winston.add(winston.transports.Logstash, {
-//   port: 10194,
-//   host: '46844509-c4df-4418-b7db-61abd1ecd859-ls.logit.io',
-//   ssl_enable: true,
-//   max_connect_retries: -1,
-// });
+});
 
 exports.echo = (req, res) => {
   if (!req.query.param) {
@@ -26,11 +19,7 @@ exports.echo = (req, res) => {
 
   const param = req.query.param;
 
-  // Not working, clone error, not supported?
-  // winston.error('This is a test error log message', { custom: 'my custom field', Environment: 'local' });
-  // winston.log("error", "This is a standard log message");
-  // winston.info("This is an info log message");
-  // winston.warn("This is a warning log message");
+  winston.info("Param:", param);
 
   res.send(param)
 };
